992f95ae6fee3be575efff0bc14b7ba49067e06b
kisikisikisi <ki.shu1125bi@gmail.com>
2020-05-22 14:48:08 +0900

    byte conversion

--- idata/799.lisp
+++ idata/799.lisp
@@ -1,3 +1,3 @@
 (setf *image-struct* '(("799" 10 10 10 10 BLUE)
-("Wikipedia" 5 5 5 5  GREEN)
+("b'Wikipedia'" 5 5 5 5  GREEN)
 ))
--- idata/837.lisp
+++ idata/837.lisp
@@ -1,5 +1,5 @@
 (setf *image-struct* '(("837" 10 10 10 10 BLUE)
-("エクスポート" 5 5 5 5  GREEN)
-("の" 20 20 20 20  GREEN)
-("管理" 35 35 35 35  GREEN)
+("b'\xe3\x82\xa8\xe3\x82\xaf\xe3\x82\xb9\xe3\x83\x9d\xe3\x83\xbc\xe3\x83\x88'" 5 5 5 5  GREEN)
+("b'\xe3\x81\xae'" 20 20 20 20  GREEN)
+("b'\xe7\xae\xa1\xe7\x90\x86'" 35 35 35 35  GREEN)
 ))
